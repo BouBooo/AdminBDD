6db22d78a730e31e5d40ac4f0dc6e161c3421026
BouBooo <35866542+BouBooo@users.noreply.github.com>
2019-12-05 16:45:24 +0100

    Update insert_data.sql

--- TP3/tp3/insert_data.sql
+++ TP3/tp3/insert_data.sql
@@ -1,6 +1,5 @@
 USE teams;
 
-INSERT INTO games (match_date, victory, observations) VALUES ('2019-5-6', 1, "RAS");
-INSERT INTO games (match_date, victory, observations) VALUES ('2019-5-7', 0, "Pas ouf");
-INSERT INTO games (match_date, victory, observations) VALUES ('2019-5-8', 1, "RAS les gars");
-
+INSERT INTO games (match_date, victory, observations) VALUES ('2019-5-6', 1, "Observation1");
+INSERT INTO games (match_date, victory, observations) VALUES ('2019-5-7', 0, "Observation2");
+INSERT INTO games (match_date, victory, observations) VALUES ('2019-5-8', 1, "Observation3");
